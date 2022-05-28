e4aa855a58f44ca429fc783c46bd027e7e213b1e
da <>
2022-05-28 23:38:47 +0200

    implement shearing

--- isometric.cpp
+++ isometric.cpp
@@ -7,6 +7,31 @@
 struct point { int x;    int y;};
 struct line  { point p1; point p2; };
 
+static float shear_x = 1;
+static float shear_y = 0;
+
+point apply_transform(point p) // apply a shear transform
+{
+    point sheared_p = {0,0};
+    float transform[9] = {      1, shear_x, 0,
+                          shear_y,       1, 0,
+                                0,       0, 1};
+    sheared_p.x = p.x * transform[0] + p.y * transform[1] + 0 * transform[2];
+    sheared_p.y = p.x * transform[3] + p.y * transform[4] + 0 * transform[5];
+    return sheared_p;
+};
+
+point apply_inverse(point p) // remove shear
+{
+    point sheared_p = {0,0}; // TODO rename
+    float inverse[9] = {(1.f / (1.f - (shear_x * shear_y))), shear_x/(shear_x*shear_y - 1.f), 0,
+                        shear_y/(shear_x*shear_y - 1.f),     1.f/(1.f - shear_x*shear_y),     0,
+                                0,       0, 1};
+    sheared_p.x = p.x * inverse[0] + p.y * inverse[1] + 0 * inverse[2];
+    sheared_p.y = p.x * inverse[3] + p.y * inverse[4] + 0 * inverse[5];
+    return sheared_p;
+};
+
 int main(int argc, char** argv)
 {
     // init sdl
@@ -48,6 +73,14 @@ int main(int argc, char** argv)
                     is_running = false;
                 }
 
+                if (event.type == SDL_KEYDOWN)
+                {
+                    if (event.key.keysym.sym == SDLK_x) { shear_x -= 0.1f; printf("shear_x: %f\n", shear_x); }
+                    if (event.key.keysym.sym == SDLK_c) { shear_x += 0.1f; printf("shear_x: %f\n", shear_x); }
+                    if (event.key.keysym.sym == SDLK_s) { shear_y -= 0.1f; printf("shear_x: %f\n", shear_y); }
+                    if (event.key.keysym.sym == SDLK_d) { shear_y += 0.1f; printf("shear_x: %f\n", shear_y); }
+                }
+
                 if (event.type == SDL_MOUSEMOTION)
                 {
                     mouse_pos.x = event.motion.x;
@@ -60,14 +93,18 @@ int main(int argc, char** argv)
         line* highlight_vert_line = NULL;
         line* highlight_hori_line = NULL;
         { /* update loop */
+            point grid_mouse = apply_inverse(mouse_pos);
+
             // find out between which lines the mouse is (NOTE "bruteforce" version for now)
             for(int i = 0; i < 12; i++)
             {
-                if (vert_lines[i].p1.x < mouse_pos.x && vert_lines[i+1].p1.x > mouse_pos.x)
+                // TODO apply the inverse of the shear transform to the mouse before
+
+                if (vert_lines[i].p1.x < grid_mouse.x && vert_lines[i+1].p1.x > grid_mouse.x)
                 {
                     highlight_vert_line = &vert_lines[i];
                 }
-                if (hori_lines[i].p1.y < mouse_pos.y && hori_lines[i+1].p1.y > mouse_pos.y)
+                if (hori_lines[i].p1.y < grid_mouse.y && hori_lines[i+1].p1.y > grid_mouse.y)
                 {
                     highlight_hori_line = &hori_lines[i];
                 }
@@ -78,13 +115,33 @@ int main(int argc, char** argv)
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
             SDL_RenderClear(renderer);
 
+
             SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
             for(int i = 0; i < 13; i++)
             {
-                SDL_RenderDrawLine(renderer, vert_lines[i].p1.x, vert_lines[i].p1.y,
-                                             vert_lines[i].p2.x, vert_lines[i].p2.y);
-                SDL_RenderDrawLine(renderer, hori_lines[i].p1.x, hori_lines[i].p1.y,
-                                             hori_lines[i].p2.x, hori_lines[i].p2.y);
+                if (highlight_vert_line == &vert_lines[i] ||
+                    highlight_vert_line == &vert_lines[i-1]) // TODO why does this not go OOB...?
+                {
+                    SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+                }
+                else { SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE); }
+
+                // apply a shear transform to the lines before rendering
+                SDL_RenderDrawLine(renderer, apply_transform(vert_lines[i].p1).x,
+                                             apply_transform(vert_lines[i].p1).y,
+                                             apply_transform(vert_lines[i].p2).x,
+                                             apply_transform(vert_lines[i].p2).y);
+                if (highlight_hori_line == &hori_lines[i] ||
+                    highlight_hori_line == &hori_lines[i-1])
+                {
+                    SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+                }
+                else { SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE); }
+
+                SDL_RenderDrawLine(renderer, apply_transform(hori_lines[i].p1).x,
+                                             apply_transform(hori_lines[i].p1).y,
+                                             apply_transform(hori_lines[i].p2).x,
+                                             apply_transform(hori_lines[i].p2).y);
             }
 
             // TODO hardcoded
@@ -93,7 +150,8 @@ int main(int argc, char** argv)
                 SDL_Rect highlight_box = { highlight_vert_line->p1.x,
                                            highlight_hori_line->p1.y,
                                            60, 48 };
-                SDL_RenderFillRect(renderer, &highlight_box);
+                // TODO use a FillPolygon version...
+                // SDL_RenderFillRect(renderer, &highlight_box);
             }
 
             SDL_RenderPresent(renderer);
