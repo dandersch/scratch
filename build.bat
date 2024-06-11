5634e7783da88ebac04b00dfd79fd2ed1a366e7c
da <>
2024-06-11 12:10:17 +0200

    update build.bat to generate pdb's

--- build.bat
+++ build.bat
@@ -13,8 +13,7 @@ exit 0
 echo 'Processing for Windows'
 
 REM build exe
-cl.exe /nologo /I ./ /I ./dep main.c /link ./dep/SDL2main.lib ./dep/SDL2.lib shell32.lib opengl32.lib ./dep/glew32.lib /SUBSYSTEM:CONSOLE /OUT:main.exe
+cl.exe /Zi /nologo /I ./ /I ./dep main.c /link ./dep/SDL2main.lib ./dep/SDL2.lib shell32.lib opengl32.lib ./dep/glew32.lib /DEBUG:FULL /PDB:main.pdb /SUBSYSTEM:CONSOLE /OUT:main.exe
 
 REM build dll
-clang-cl.exe /nologo /I ./ /I ./dep hot_reload.c /LD /link opengl32.lib ./dep/glew32.lib /NOIMPLIB /NOEXP /OUT:code.dll
-
+clang-cl.exe /Zi /nologo /I ./ /I ./dep hot_reload.c /LD /link opengl32.lib ./dep/glew32.lib /NOIMPLIB /NOEXP /DEBUG:FULL /PDB:code.pdb /OUT:code.dll
