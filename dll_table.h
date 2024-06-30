32bffde17764119727497b6a3a365ee0a8eb0601
da <>
2024-06-30 04:27:04 +0200

    add working implementation

--- dll_table.h
+++ dll_table.h
@@ -1,39 +1,45 @@
-/* USAGE CODE: define x-macros before including dll_table.h */
-#define RECODE_DLL_TABLE(DLL) \
-    DLL(sound, "code.dll",  SOUND_FUNCTION_TABLE) \
-    DLL(grafx, "grafx.dll", GRAFX_FUNCTION_TABLE)
-    
-#define GRAFX_FUNCTION_TABLE(FUNC, DLL) \
-    FUNC(DLL, void, init_graphics, int) \
-    FUNC(DLL, void, draw, void)
-
-#define SOUND_FUNCTION_TABLE(FUNC, DLL) \
-    FUNC(DLL, void, init_sound, int) \
-    FUNC(DLL, void, play_sound, void)
-// #include "dll_table.h"
+#pragma once
+/* TODO put in implementation */
+#include <sys/stat.h>
+#include <dlfcn.h> // NOTE linux only
 
 /* static function pointers */
 #define FUNC_POINTERS(dll, ret, name, ...) static ret (*name)(__VA_ARGS__);
 #define GRAB_TABLE(dll, path, table, ...) table(FUNC_POINTERS,_)
 RECODE_DLL_TABLE(GRAB_TABLE)
 
-/* static dll handles */
+/* static dll handles + modification times */
+#define DLL_HANDLE(dll, path, table, ...) static void* dll##_dll_handle; static time_t dll##_last_mod;
+RECODE_DLL_TABLE(DLL_HANDLE)
 
 /* function declarations */
 #define MAIN_FUNCTIONS(dll, path, table, ...)     \
-                void dll##_dll_changed_on_disk(); \
-                void dll##_dll_load();            \
-                void dll##_dll_load_functions();  \
+                int  dll##_dll_changed_on_disk(); \
+                int  dll##_dll_open();            \
+                void dll##_dll_close();           \
+                void dll##_dll_load_functions();
 RECODE_DLL_TABLE(MAIN_FUNCTIONS)
 
 #ifdef DLL_TABLE_IMPLEMENTATION
 
-/* dll_load_functions implementation */
-#define LOAD_FUNCTIONS(dll, ret, name, ...) name = (__VA_ARGS__) SDL_LoadFunction(dll, #name);
-#define IMPLEMENT_DLL_LOAD_FUNCTIONS(dll, path, table, ...) void dll##_dll_load_functions() { table(LOAD_FUNCTIONS, dll) };
-RECODE_DLL_TABLE(IMPLEMENT_DLL_LOAD_FUNCTIONS) 
+/* dll_open implementation */
+#define IMPLEMENT_DLL_OPEN(dll, path, table, ...) int dll##_dll_open() { dll##_dll_handle = dlopen(path, RTLD_NOW); struct stat attr; stat(path, &attr); dll##_last_mod = attr.st_mtime; if (!dll##_dll_handle) { return 0; } else { return 1; } };
+RECODE_DLL_TABLE(IMPLEMENT_DLL_OPEN)
 
+/* dll_close implementation */ // TODO should also set function pointers to NULL
+#define IMPLEMENT_DLL_CLOSE(dll, path, table, ...) void dll##_dll_close() { dlclose(dll##_dll_handle); };
+RECODE_DLL_TABLE(IMPLEMENT_DLL_CLOSE)
+
+/* dll_changed_on_disk implementation */
+#define IMPLEMENT_DLL_CHANGED(dll, path, table, ...) int dll##_dll_changed_on_disk() { struct stat attr; int ret = ((stat(path, &attr) == 0) && (dll##_last_mod != attr.st_mtime)); return ret; }
+RECODE_DLL_TABLE(IMPLEMENT_DLL_CHANGED)
+
+/* dll_load_functions implementation */
+// function pointer cast : (ret (*)(__VA_ARGS__))
+#define LOAD_FUNCTIONS(dll, ret, name, ...) name =  dlsym(dll##_dll_handle, #name);
+#define IMPLEMENT_DLL_LOAD_FUNCTIONS(dll, path, table, ...) void dll##_dll_load_functions() { table(LOAD_FUNCTIONS, dll) };
+RECODE_DLL_TABLE(IMPLEMENT_DLL_LOAD_FUNCTIONS)
 
 #endif
-
 #undef LOAD_FUNCTIONS
+// TODO undef everything
